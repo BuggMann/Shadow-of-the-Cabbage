1c6f798764d28ceb332a1cbf0db04cca1ce8107d
heapc_000 <heapc_000@10.18.151.154>
2016-03-10 12:29:44 -0700

    3/10/2016 created the actor enum list

--- shadowOfTheCabbage/src/byui/cit260/shadowOfTheCabbage/model/Actor.java
+++ shadowOfTheCabbage/src/byui/cit260/shadowOfTheCabbage/model/Actor.java
@@ -5,6 +5,7 @@
  */
 package byui.cit260.shadowOfTheCabbage.model;
 
+import java.awt.Point;
 import java.io.Serializable;
 import java.util.Objects;
 
@@ -12,78 +13,30 @@ import java.util.Objects;
  *
  * @author heapc_000
  */
-public class Actor implements Serializable {
-    
-    private String actorName;
-    private String description;
-    private double location;
+public enum Actor implements Serializable {
+   Eponine("She is your lovely lady friend."),
+   HoboJoe("He is a very old and wise guy");
+    private final String description;
+    private final Point location;
 
-    public Actor() {
-    }
-    
-    public String getActorName() {
-        return actorName;
-    }
-
-    public void setActorName(String actorName) {
-        this.actorName = actorName;
+    Actor(String description) {
+        this.description = description;
+        location = new Point(1,1);
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getLocation() {
+    public Point getLocation() {
         return location;
-    }
-
-    public void setLocation(double location) {
-        this.location = location;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 11 * hash + Objects.hashCode(this.actorName);
-        hash = 11 * hash + Objects.hashCode(this.description);
-        hash = 11 * hash + (int) (Double.doubleToLongBits(this.location) ^ (Double.doubleToLongBits(this.location) >>> 32));
-        return hash;
     }
 
     @Override
     public String toString() {
-        return "Actor{" + "name=" + actorName + ", description=" + description + ", location=" + location + '}';
+        return "Actor{" + "description=" + description + ", location=" + location + '}';
     }
-    
-    
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Actor other = (Actor) obj;
-        if (Double.doubleToLongBits(this.location) != Double.doubleToLongBits(other.location)) {
-            return false;
-        }
-        if (!Objects.equals(this.actorName, other.actorName)) {
-            return false;
-        }
-        if (!Objects.equals(this.description, other.description)) {
-            return false;
-        }
-        return true;
-    }
-    
+   
     
 }
